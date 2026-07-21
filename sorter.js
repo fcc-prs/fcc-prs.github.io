e7bc91df15055171b30cd9ecd79e8487d65f6f81
David Lange <David.Lange@cern.ch>
2026-07-21 15:10:22 -0500

    Update sorter function to filter by all table cells
    Refactor sorter function to check all table cells for filtering.

--- sorter.js
+++ sorter.js
@@ -1,6 +1,6 @@
 function sorter() {
   // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
+  var input, filter, table, tr, td, i, txtValue, tds;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("pr-table");
@@ -8,13 +8,16 @@ function sorter() {
   console.log(tr.length);
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
+    tds = tr[i].getElementsByTagName("td") //[0];
+    for (j = 0; j< tds.length; j++ ) {
+      td = tds[j];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
       }
     }
   }
