de21698034346fc555a117dd8824818d82f07425
David Lange <David.Lange@cern.ch>
2026-07-21 15:13:05 -0500

    Refactor sorter function to track matches

--- sorter.js
+++ sorter.js
@@ -1,6 +1,6 @@
 function sorter() {
   // Declare variables
-  var input, filter, table, tr, td, i, txtValue, tds;
+  var input, filter, table, tr, td, i, txtValue, tds, matches;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("pr-table");
@@ -9,16 +9,20 @@ function sorter() {
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     tds = tr[i].getElementsByTagName("td") //[0];
+    matches=0;
     for (j = 0; j< tds.length; j++ ) {
       td = tds[j];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+          matches=1;
         }
       }
+    }
+    if ( matches==1 ) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
     }
   }
 }
